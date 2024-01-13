d3487c820299602576cb74876c7683cb3455383c
lkemanov1 <lkemanov2@gmail.com>
2024-01-14 00:58:07 +0200

    generate mock data for cars

--- src/main/java/com/sofiaexport/mockdata/MockDataImporter.java
+++ src/main/java/com/sofiaexport/mockdata/MockDataImporter.java
@@ -2,8 +2,10 @@ package com.sofiaexport.mockdata;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.sofiaexport.commands.AddCarCommand;
 import com.sofiaexport.commands.AddUserCommand;
 import com.sofiaexport.model.Role;
+import com.sofiaexport.service.CarService;
 import com.sofiaexport.service.UserService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -21,22 +23,38 @@ import java.util.List;
 public class MockDataImporter implements CommandLineRunner {
     private final Gson gson;
     private final UserService userService;
+    private final CarService carService;
 
     private final static String USERS_DATE_FILE_PATH = "src/main/resources/mockdata/users.json";
+    private final static String CARS_DATE_FILE_PATH = "src/main/resources/mockdata/cars.json";
 
     @Override
     public void run(String... args) {
-        List<AddUserCommand> userResponses = readUserDataFromJson()
+        List<AddUserCommand> mockUsers = readUserDataFromJson()
                 .stream()
                 .map(RegisterUserCommand::toModel)
                 .toList();
 
-        userResponses.parallelStream().forEach(userService::register);
+        mockUsers.forEach(userService::register);
+
+        List<AddCarCommand> mockCars = readCarDataFromJson();
+        mockCars.forEach(carService::addCar);
     }
 
     private List<RegisterUserCommand> readUserDataFromJson() {
         try (FileReader reader = new FileReader(USERS_DATE_FILE_PATH)) {
             TypeToken<List<RegisterUserCommand>> typeToken = new TypeToken<>() {};
+
+            return gson.fromJson(reader, typeToken.getType());
+        } catch (Exception e) {
+            log.error("Error deserializing json", e);
+        }
+        return Collections.emptyList();
+    }
+
+    private List<AddCarCommand> readCarDataFromJson() {
+        try (FileReader reader = new FileReader(CARS_DATE_FILE_PATH)) {
+            TypeToken<List<AddCarCommand>> typeToken = new TypeToken<>() {};
 
             return gson.fromJson(reader, typeToken.getType());
         } catch (Exception e) {
