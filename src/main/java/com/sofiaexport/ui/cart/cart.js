e321b45d94597eaf24ee756501d0946e7501a8f6
Edis Hasanov <sngedobeat@gmail.com>
2024-01-15 22:10:11 +0200

    [Cart] Add cart page implementation

---
+++ src/main/java/com/sofiaexport/ui/cart/cart.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var userId = localStorage.getItem("userId");
+
+    if (!userId) {
+        console.error("User ID not found in local storage.");
+        return;
+    }
+
+    fetch("http://localhost:8080/api/v1/user/" + userId + "/order")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error fetching shopping cart items");
+            }
+            return response.json();
+        })
+        .then(data => {
+            var cartItemsContainer = document.getElementById("cartItems");
+
+            var sumHtml = `<p class="sum"><strong>Total Sum:</strong> ${data.sum}</p>`;
+            cartItemsContainer.insertAdjacentHTML('beforeend', sumHtml);
+
+            data.autoparts.forEach(function (autoPart) {
+                var autoPartHtml = `
+                    <li class="autoPart">
+                        <div class="autoPartInfo">
+                            <p class="autoPartTitle"><strong>${autoPart.brand} - ${autoPart.partType}</strong></p>
+                            <p class="autoPartPrice">Price: ${autoPart.price}</p>
+                        </div>
+                    </li>
+                `;
+                cartItemsContainer.insertAdjacentHTML('beforeend', autoPartHtml);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
