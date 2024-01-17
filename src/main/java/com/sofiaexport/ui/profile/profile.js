5e0a734d87e0e19760738e5e536115e412330be5
Edis Hasanov <sngedobeat@gmail.com>
2024-01-17 03:01:32 +0200

    [Profile] Include order history in profile page

--- src/main/java/com/sofiaexport/ui/profile/profile.js
+++ src/main/java/com/sofiaexport/ui/profile/profile.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (!response.ok) {
                 throw new Error('Network response was not ok.');
             }
-            return response.json()
+            return response.json();
         })
         .then(data => {
             document.getElementById('profilePicture').src = 'https://www.gravatar.com/avatar/' + md5(data.email);
@@ -20,7 +20,41 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('name').textContent = data.name;
         })
         .catch(error => {
-            console.error('Error during registration:', error);
+            console.error('Error during fetching user profile:', error);
+        });
+
+    fetch(`http://localhost:8080/api/v1/user/${localStorage.getItem('userId')}/orders`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            return response.json();
+        })
+        .then(orderHistory => {
+            const ordersList = document.getElementById('ordersList');
+
+            if (orderHistory.length === 0) {
+                ordersList.innerHTML = '<p>No order history available.</p>';
+            } else {
+                orderHistory.forEach(order => {
+                    const orderHtml = `
+                    <li class="order">
+                        <p>Order Sum: ${order.sum}</p>
+                        <p>Order Status: ${order.orderStatus}</p>
+                    </li>
+                `;
+                    ordersList.insertAdjacentHTML('beforeend', orderHtml);
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Error during fetching user order history:', error);
         });
 });
 
