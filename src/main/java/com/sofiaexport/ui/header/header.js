efb1edeb0cb901b770c1dad4a11b05d843040bb8
Edis Hasanov <sngedobeat@gmail.com>
2024-01-17 02:27:43 +0200

    [Logout] Add logout functionality

--- src/main/java/com/sofiaexport/ui/header/header.js
+++ src/main/java/com/sofiaexport/ui/header/header.js
@@ -3,16 +3,24 @@ function checkToken() {
     var loginButton = document.getElementById('login-btn');
     var cartButton = document.getElementById('cart-btn');
     var profileButton = document.getElementById('profile-btn');
+    var logoutButton = document.getElementById('logout-btn');
 
     if (token == null) {
         // User is not logged in, show the login button and hide the profile button
         loginButton.style.display = 'inline-block';
         cartButton.style.display = 'none';
         profileButton.style.display = 'none';
+        logoutButton.style.display = 'none';
     } else {
         // User is logged in, hide the login button and show the profile button
         loginButton.style.display = 'none';
         cartButton.style.display = 'inline-block';
         profileButton.style.display = 'inline-block';
+        logoutButton.style.display = 'inline-block';
     }
+
+    logoutButton.addEventListener('click', function () {
+        localStorage.removeItem('accessToken');
+        window.location.href = "../home/home-page.html";
+    });
 }
